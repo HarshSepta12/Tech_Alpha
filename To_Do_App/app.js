103ba445f355c71514b0f8ffeda4b2cc96fbcc32
harshservi48@gmail.com <harshservi48@gmail.com>
2024-06-12 23:17:36 +0530

    In Portfolio media Query Add

--- To_Do_App/app.js
+++ To_Do_App/app.js
@@ -1,9 +1,7 @@
 const item = document.querySelector("#item");
 const toDoBox = document.querySelector("#to-do-box");
 
-item.addEventListener(
-    "keyup",
-    function(event){
+item.addEventListener("keyup",function(event){
         if(event.key == "Enter"){
             addtodo(this.value);
             this.value = "";
@@ -13,19 +11,12 @@ item.addEventListener(
 
 const addtodo = (item) =>{
     const listitem = document.createElement("li");
-    listitem.innerHTML = `
-    ${item}
-    <i class="fa-solid fa-xmark"></i>
-    `;
-    listitem.addEventListener(
-        "click",
-        function(){
+    listitem.innerHTML = `${item}<i class="fa-solid fa-xmark"></i>`;
+    listitem.addEventListener("click",function(){
             this.classList.toggle("done");
         }
     )
-    listitem.querySelector("i").addEventListener(
-        "click",
-        function(){
+    listitem.querySelector("i").addEventListener("click",function(){
             listitem.remove()
         }
     )
